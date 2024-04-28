f213db799bd0b93bb8d7a0cba2bafb24e0742d21
Yun-tellije <dbswjddms517@naver.com>
2024-04-29 01:17:47 +0900

    백준 - 2675번 문자열 반복

---
+++ src/main/java/algorithm/backjoon/B2_2675.java
@@ -0,0 +1,25 @@
+package algorithm.backjoon;
+
+import java.util.Scanner;
+
+public class B2_2675 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            int num = sc.nextInt();
+            String s = sc.next();
+
+            for (int j = 0; j < s.length(); j++) {
+                for (int k = 0; k < num; k++) {
+                    System.out.print(s.charAt(j));
+                }
+            }
+            System.out.println();
+        }
+
+        sc.close();
+    }
+}
