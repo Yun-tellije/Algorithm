85b2ed8b83c947262577864324ce53b189a36e91
Yun-tellije <dbswjddms517@naver.com>
2024-04-29 01:20:51 +0900

    백준 - 27866번_문자와 문자열

---
+++ src/main/java/algorithm/backjoon/B5_27866.java
@@ -0,0 +1,13 @@
+package algorithm.backjoon;
+
+import java.util.Scanner;
+
+public class B5_27866 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        int n = sc.nextInt();
+
+        System.out.println(s.charAt(n-1));
+    }
+}
