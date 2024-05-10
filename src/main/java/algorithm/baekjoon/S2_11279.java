8370f519c4bd582047f414176a63783817fb7ac5
Yun-tellije <dbswjddms517@naver.com>
2024-05-10 23:29:55 +0900

    백준 - 11279번_최대 힙(우선순위 큐)

---
+++ src/main/java/algorithm/baekjoon/S2_11279.java
@@ -0,0 +1,34 @@
+package algorithm.baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Collections;
+import java.util.PriorityQueue;
+
+public class S2_11279 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int n = Integer.parseInt(br.readLine());
+
+        PriorityQueue<Integer> arr = new PriorityQueue<>(Collections.reverseOrder());
+
+        for (int i = 0; i < n; i++) {
+            int t = Integer.parseInt(br.readLine());
+            if (t == 0) {
+                if (arr.isEmpty()) {
+                    sb.append(0).append("\n");
+                }
+                else {
+                    sb.append(arr.remove()).append("\n");
+                }
+            } else {
+                arr.add(t);
+            }
+        }
+
+        System.out.println(sb);
+    }
+}
