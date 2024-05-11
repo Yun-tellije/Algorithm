d7a347d2f9d93ced700876bf7ed4baa0f16e908e
Yun-tellije <dbswjddms517@naver.com>
2024-05-12 01:22:00 +0900

    백준 - 2667번_단지번호붙이기(DFS)

--- src/main/java/algorithm/baekjoon/S1_2667.java
+++ src/main/java/algorithm/baekjoon/S1_2667.java
@@ -16,6 +16,7 @@ public class S1_2667 {
     static PriorityQueue<Integer> list = new PriorityQueue<>();
     static int[] dx = {0, 0, -1, 1};
     static int[] dy = {1, -1, 0, 0};
+    static int cnt;
 
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -36,8 +37,10 @@ public class S1_2667 {
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
                 if (arr[i][j] == 1 && !visit[i][j]) {
-                    bfs(i, j);
+                    cnt = 1;
+                    dfs(i, j);
                     count++;
+                    list.add(cnt);
                 }
             }
         }
@@ -51,29 +54,18 @@ public class S1_2667 {
         System.out.println(sb);
     }
 
-    static void bfs(int x, int y) {
-        Queue<int[]> queue = new LinkedList<>();
-        queue.add(new int[] {x, y});
-        int cnt = 1;
+    static void dfs(int x, int y) {
+        visit[x][y] = true;
 
-        while (!queue.isEmpty()) {
-            int a = queue.peek()[0];
-            int b = queue.peek()[1];
-            visit[a][b] = true;
-            queue.poll();
-
-            for (int i = 0; i < 4; i++) {
-                int cx = a + dx[i];
-                int cy = b + dy[i];
-                if (cx >= 0 && cx < n && cy >= 0 && cy < n) {
-                    if (arr[cx][cy] == 1 && !visit[cx][cy]) {
-                        queue.add(new int[] {cx, cy});
-                        visit[cx][cy] = true;
-                        cnt++;
-                    }
+        for (int i = 0; i < 4; i++) {
+            int cx = x + dx[i];
+            int cy = y + dy[i];
+            if (cx >= 0 && cx < n && cy >= 0 && cy < n) {
+                if (arr[cx][cy] == 1 && !visit[cx][cy]) {
+                    cnt++;
+                    dfs(cx, cy);
                 }
             }
         }
-        list.add(cnt);
     }
 }
