8a5f58e7a01b8fe4312c1f15522c3b41168f9a12
Yun-tellije <dbswjddms517@naver.com>
2024-05-16 01:51:59 +0900

    백준 - 11053번_가장 긴 증가하는 부분 수열(DP)

---
+++ src/main/java/algorithm/baekjoon/S1_11053.java
@@ -0,0 +1,42 @@
+package algorithm.baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class S1_11053 {
+
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+        int[] dp = new int[n];
+        st = new StringTokenizer(br.readLine());
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        for (int i = 0; i < n; i++) {
+            dp[i] = 1;
+
+            for (int j = 0; j < i; j++) {
+                if (arr[j] < arr[i] && dp[i] < dp[j] + 1) {
+                    dp[i] = dp[j] + 1;
+                }
+            }
+        }
+
+        int M = 0;
+        for (int i = 0; i < n; i++) {
+            if (dp[i] > M)
+                M = dp[i];
+        }
+
+        sb.append(M);
+        System.out.println(sb);
+    }
+}
