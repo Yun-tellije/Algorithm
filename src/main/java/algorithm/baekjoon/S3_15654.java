09aca156c3a6494136935dec1f17aed06e479ad0
Yun-tellije <dbswjddms517@naver.com>
2024-05-19 00:58:56 +0900

    백준 - 15654번_N과 M (5)(백트래킹)

---
+++ src/main/java/algorithm/baekjoon/S3_15654.java
@@ -0,0 +1,58 @@
+package algorithm.baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class S3_15654 {
+
+    static int n;
+    static int m;
+    static int[] arr;
+    static int[] answer;
+    static boolean[] visit;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        arr = new int[n];
+        answer = new int[m];
+        visit = new boolean[n];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+
+        dfs(0);
+
+        System.out.println(sb);
+    }
+
+    static void dfs(int depth) {
+        if (depth == m) {
+            for (int val : answer) {
+                sb.append(val).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for (int i = 0; i < n; i++) {
+            if (!visit[i]) {
+                visit[i] = true;
+                answer[depth] = arr[i];
+                dfs(depth + 1);
+                visit[i] = false;
+            }
+
+        }
+    }
+}
