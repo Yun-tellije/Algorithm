21d3a8cf9a3f5cf9682216dab36492a8df9df360
Yun-tellije <dbswjddms517@naver.com>
2024-05-19 00:59:50 +0900

    백준 - 11660번_구간 합 구하기 5(누적합, DP)

---
+++ src/main/java/algorithm/baekjoon/S1_11660.java
@@ -0,0 +1,52 @@
+package algorithm.baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class S1_11660 {
+
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int[][] arr = new int[n][n];
+        int[][] dp = new int[n][n];
+
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < n; j++) {
+                arr[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            dp[i][0] = arr[i][0];
+            for (int j = 1; j < n; j++) {
+                dp[i][j] = dp[i][j - 1] + arr[i][j];
+            }
+        }
+
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int x1 = Integer.parseInt(st.nextToken()) - 1;
+            int y1 = Integer.parseInt(st.nextToken()) - 1;
+            int x2 = Integer.parseInt(st.nextToken()) - 1;
+            int y2 = Integer.parseInt(st.nextToken()) - 1;
+            int sum = 0;
+
+            for (int j = x1; j <= x2; j++) {
+                sum += dp[j][y2];
+                if (y1 > 0) {
+                    sum -= dp[j][y1 - 1];
+                }
+            }
+
+            sb.append(sum).append("\n");
+        }
+        System.out.println(sb);
+    }
+}
