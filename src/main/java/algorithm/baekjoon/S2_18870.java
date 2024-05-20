71cd7ec4ba8147ed7ec2c1e97fbd004fa247055c
Yun-tellije <dbswjddms517@naver.com>
2024-05-21 02:01:53 +0900

    백준 - 18870번_좌표 압축(좌표압축)

---
+++ src/main/java/algorithm/baekjoon/S2_18870.java
@@ -0,0 +1,43 @@
+package algorithm.baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.StringTokenizer;
+
+public class S2_18870 {
+
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int n = Integer.parseInt(br.readLine());
+
+        int[] arr = new int[n];
+        int[] sorted = new int[n];
+        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = sorted[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(sorted);
+
+        int rank = 0;
+
+        for (int val : sorted) {
+            if(!map.containsKey(val)) {
+                map.put(val, rank);
+                rank++;
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            sb.append(map.get(arr[i])).append(" ");
+        }
+
+        System.out.println(sb);
+    }
+}
