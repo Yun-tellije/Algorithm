e2ff173cbed1120e6d31cef4956e0e103534f0bd
Yun-tellije <dbswjddms517@naver.com>
2024-08-28 00:53:18 +0900

    [브루트포스] 안전 영역(dfs 풀이)

--- src/main/java/algorithm/bruteforce/S1_2468.java
+++ src/main/java/algorithm/bruteforce/S1_2468.java
@@ -3,8 +3,6 @@ package algorithm.bruteforce;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class S1_2468 {
@@ -42,7 +40,7 @@ public class S1_2468 {
             for (int j = 0; j < n; j++) {
                 for (int k = 0; k < n; k++) {
                     if (!visit[j][k] && arr[j][k] > i) {
-                        bfs(new int[] {j, k}, i);
+                        dfs(j, k, i);
                         cnt++;
                     }
                 }
@@ -54,24 +52,16 @@ public class S1_2468 {
         System.out.println(sb);
     }
 
-    static void bfs(int[] start, int idx) {
-        Queue<int[]> queue = new LinkedList<>();
-        queue.add(start);
+    static void dfs(int x, int y, int idx) {
+        visit[x][y] = true;
 
-        while (!queue.isEmpty()) {
-            int bx = queue.peek()[0];
-            int by = queue.peek()[1];
-            queue.poll();
+        for (int i = 0; i < 4; i++) {
+            int cx = x + dx[i];
+            int cy = y + dy[i];
 
-            for (int i = 0; i < 4; i++) {
-                int cx = bx + dx[i];
-                int cy = by + dy[i];
-
-                if (cx >= 0 && cx < n && cy >= 0 && cy < n) {
-                    if (arr[cx][cy] > idx && !visit[cx][cy]) {
-                        queue.add(new int[] {cx, cy});
-                        visit[cx][cy] = true;
-                    }
+            if (cx >= 0 && cx < n && cy >= 0 && cy < n) {
+                if (arr[cx][cy] > idx && !visit[cx][cy]) {
+                    dfs(cx, cy, idx);
                 }
             }
         }
