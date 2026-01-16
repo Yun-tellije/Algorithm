a8664ad43fe4709d326b874e9426c76741d74bad
Jungeun Yun <104592745+Yun-tellije@users.noreply.github.com>
2026-01-16 14:47:10 +0900

    [level 3] Title: 이중우선순위큐, Time: 100.62 ms, Memory: 149 MB -BaekjoonHub

---
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
@@ -0,0 +1,47 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(String[] operations) {
+        int[] answer = new int[2];
+        
+        PriorityQueue<Integer> minPq = new PriorityQueue<>();
+        PriorityQueue<Integer> maxPq = new PriorityQueue<>((o1, o2) -> o2 - o1);
+        
+        int size = 0;
+        
+        for (int i = 0; i < operations.length; i++) {
+            String[] arr = operations[i].split(" ");
+            
+            if (arr[0].equals("I")) {
+                int num = Integer.parseInt(arr[1]);
+                
+                minPq.add(num);
+                maxPq.add(num);
+                size++;
+            } else {
+                if (size == 0) continue;
+                
+                int num = Integer.parseInt(arr[1]);
+                
+                if (num == 1) {
+                    int max = maxPq.poll();
+                    minPq.remove(max);
+                } else {
+                    int min = minPq.poll();
+                    maxPq.remove(min);
+                }
+                size--;
+            }
+            
+        }
+        
+        if (size == 0) {
+            answer[0] = answer[1] = 0;
+        } else {
+            answer[0] = maxPq.poll();
+            answer[1] = minPq.poll();
+        }
+        
+        return answer;
+    }
+}
