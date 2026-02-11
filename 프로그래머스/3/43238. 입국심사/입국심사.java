77be7abdbfd49d695f70f0555401f5790f10fd37
Jungeun Yun <104592745+Yun-tellije@users.noreply.github.com>
2026-02-11 15:26:30 +0900

    [level 3] Title: 입국심사, Time: 42.91 ms, Memory: 95.4 MB -BaekjoonHub

---
+++ 프로그래머스/3/43238. 입국심사/입국심사.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+class Solution {
+    public long solution(int n, int[] times) {        
+        long left = 1;
+        long right = 0;
+        for (int i : times) {
+            right = Math.max(i, right);
+        }
+        right *= n;
+        long answer = 0;
+        
+        while (left <= right) {
+            long mid = (left + right) / 2;
+            
+            long sum = 0;
+            for (int i : times) {
+                sum += mid / i;
+                
+                if (sum >= n) break;
+            }
+            
+            if (sum >= n) {
+                answer = mid;
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        
+        return answer;
+    }
+}
