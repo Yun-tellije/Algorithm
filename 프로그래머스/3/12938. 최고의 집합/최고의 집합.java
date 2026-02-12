dfe79a130a6084d5d76df11a2c19188173856eec
Jungeun Yun <104592745+Yun-tellije@users.noreply.github.com>
2026-02-12 23:11:54 +0900

    [level 3] Title: 최고의 집합, Time: 0.31 ms, Memory: 56 MB -BaekjoonHub

---
+++ 프로그래머스/3/12938. 최고의 집합/최고의 집합.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int n, int s) {
+        if (s < n) {
+            return new int[]{-1};
+        }
+        
+        int[] answer = new int[n];
+        
+        int base = s / n;
+        int remain = s % n;
+        
+        for (int i = 0; i < n; i++) {
+            answer[i] = base;
+        }
+        
+        for (int i = n - 1; i >= n - remain; i--) {
+            answer[i]++;
+        }
+        
+        return answer;
+    }
+}
